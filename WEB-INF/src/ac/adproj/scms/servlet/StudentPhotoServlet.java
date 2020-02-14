81537f6925df02a7a9d0f8c27fbeba9537ebee8b
Andy Cheung <micro_zx@msn.cn>
2020-02-15 02:15:29 +0800

    Add StudentPhotoServlet.java to display students' photos.

---
+++ WEB-INF/src/ac/adproj/scms/servlet/StudentPhotoServlet.java
@@ -0,0 +1,69 @@
+/*
+    Copyright (C) 2011-2020 Andy Cheung
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package ac.adproj.scms.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
+import ac.adproj.scms.dao.*;
+
+public class StudentPhotoServlet extends HttpServlet
+{
+	private static final String QUERY_SQL = "select photo as p from xs where stuid=?;";
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws IOException
+	{
+		String id = request.getParameter("id");
+
+		try (DBDao daoO = InitServlet.daoO)
+		{
+			/*     Get the photo file    */
+			Connection conn = daoO.getConnection();
+
+			PreparedStatement ps = conn.prepareStatement(QUERY_SQL);
+
+			ps.setString(1, id);
+
+			System.out.println(ps.toString());
+
+			ResultSet resSet = ps.executeQuery();
+
+			resSet.next();
+
+			Blob photoO = resSet.getBlob("p");
+
+			InputStream photoS = photoO.getBinaryStream();
+
+			byte[] buffer = photoS.readAllBytes();
+
+			/*    Send Response     */
+
+			response.setContentType("image/png");
+			response.getOutputStream().write(buffer);
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
