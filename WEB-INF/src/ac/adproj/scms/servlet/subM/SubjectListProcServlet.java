c5ba4853e5231ce3e63e094dfe520702f8ad7382
Andy Cheung <micro_zx@msn.cn>
2020-02-17 01:53:09 +0800

    [Structure] Separate the controlling logic from subM/index.jsp to SubjectListProcServlet.java.

---
+++ WEB-INF/src/ac/adproj/scms/servlet/subM/SubjectListProcServlet.java
@@ -0,0 +1,98 @@
+/*
+    Copyright (C) 2011-2020 Andy Cheung
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package ac.adproj.scms.servlet.subM;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.Writer;
+import java.sql.*;
+import java.util.*;
+
+import ac.adproj.scms.dao.*;
+import ac.adproj.scms.servlet.InitServlet;
+
+public class SubjectListProcServlet extends HttpServlet { // /scms/subM/listProc
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+
+		try (DBDao daoO = InitServlet.daoO;
+				Connection conn = daoO.getConnection();
+				PreparedStatement ps = conn.prepareStatement("delete from kc where kc.courseID = ?;");
+				Writer out = response.getWriter()) {
+
+			String delF = request.getParameter("del");
+			StringBuilder undoneMessageBuilder = new StringBuilder();
+			undoneMessageBuilder.append("如下课程由于此前输入了成绩，不能删除。\\n\\n课程号：\\n");
+			boolean notFullyDel = false;
+
+			if (delF != null) {
+				Map<String, String[]> paramMap = request.getParameterMap();
+				HashSet<String> delKeys = new HashSet<>();
+
+				for (Map.Entry<String, String[]> s : paramMap.entrySet()) {
+					if (s.getKey().equals("del"))
+						continue;
+					if (s.getValue()[0].toLowerCase().equals("on"))
+						delKeys.add(s.getKey());
+				}
+
+				for (String s : delKeys) {
+					ps.setString(1, s);
+
+					try {
+						ps.execute();
+					} catch (java.sql.SQLIntegrityConstraintViolationException e) {
+						notFullyDel = true;
+						undoneMessageBuilder.append(s);
+						undoneMessageBuilder.append("\\n");
+					}
+				}
+
+				if (notFullyDel) {
+					out.write("<script>alert(\"" + new String(undoneMessageBuilder.toString().getBytes(), "utf-8") + "\"); location.href=\"index.jsp\";</script>");
+				}
+				else
+				{
+					out.write(new String(("<script>alert(\"删除成功! \"); location.href=\"index.jsp\";</script>").getBytes(), "utf-8"));
+				}
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setHeader("Allow", "GET, HEAD, POST, OPTIONS");
+		request.setCharacterEncoding("utf-8");
+		response.setBufferSize(8192);
+		response.setContentType("text/html; charset=utf-8");
+
+		byte[] b = ("<p>测试 Test:" + request.getParameter("test") + "</p>").getBytes();
+
+		response.getWriter().print(new String(b, "utf-8"));
+	}
+}
