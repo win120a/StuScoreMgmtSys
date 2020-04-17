4bdd2f1563804660d55962db2d410d2b2ccc8201
Andy Cheung <micro_zx@msn.cn>
2020-04-18 01:45:25 +0800

    Rename DAOs (2).

---
+++ WEB-INF/src/ac/adproj/scms/dao/CourseDAO.java
@@ -0,0 +1,120 @@
+/*
+    Copyright (C) 2011-2020 Andy Cheung
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package ac.adproj.scms.dao;
+
+import ac.adproj.scms.entity.Course;
+import ac.adproj.scms.entity.Entity;
+import ac.adproj.scms.servlet.InitServlet;
+import ac.adproj.scms.servlet.ServletProcessingException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
+public final class CourseDAO {
+    private CourseDAO() {
+        // NOOP
+    }
+
+    /**
+     * Returns a Course object according to the DB's record.
+     *
+     * @param courseid The course ID.
+     * @return The Student object, or null if the student object did not exist.
+     */
+    public static Course getCourseObjectThroughDB(String courseid) {
+        try (DBDao daoO = InitServlet.daoO) {
+            ResultSet rs = daoO.query("select * from kc where courseID=?;"
+                    , courseid);
+
+            if (!rs.next()) {
+                return null;
+            }
+
+            String courseName = rs.getString("courseName");
+            String courseID = rs.getString("courseID");
+            String term = rs.getString("term");
+            int credits = rs.getInt("credits");
+            int courseHours = rs.getInt("courseHours");
+
+            Course c = new Course(courseID);
+            c.setName(courseName);
+            c.setCourseHours(courseHours);
+            c.setTerm(term);
+            c.setCredits(credits);
+
+            return c;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new ServletProcessingException(e);
+        }
+    }
+
+    /**
+     * Upload the Course object to the database.
+     *
+     * @param c The Course object that needs to upload.
+     * @throws SQLException If SQL ran into error.
+     */
+    public static void writeCourseObjectToDatabase(Course c) throws SQLException {
+        try (DBDao daoO = InitServlet.daoO) {
+
+            if (daoO.query("select term from kc where courseID=?;", c.getId()).next()) {
+                daoO.update("update kc set courseName=?, term=?, credits=?, "
+                                + "courseHours=? where courseID=?;"
+                        , c.getName()
+                        , c.getTerm()
+                        , Integer.toString(c.getCredits())
+                        , Integer.toString(c.getCourseHours())
+                        , c.getId());
+            } else {
+                daoO.insert("insert into kc values (?, ?, ?, ?, ?);"
+                        , c.getId()
+                        , c.getName()
+                        , c.getTerm()
+                        , Integer.toString(c.getCourseHours())
+                        , Integer.toString(c.getCredits()));
+            }
+        }
+    }
+
+    public static void deleteObject(String id) throws SQLException {
+        try (DBDao daoO = InitServlet.daoO) {
+            daoO.delete("delete from kc where kc.courseId=?;", id);
+        }
+    }
+
+    public static void deleteObject(Entity e) throws SQLException {
+        deleteObject(e.getId());
+    }
+
+    public static Set<String> getCourseIDSet() throws SQLException {
+        HashSet<String> courseIDSet = new HashSet<>();
+
+        try (DBDao daoO = InitServlet.daoO) {
+            ResultSet rs = daoO.query("select courseID from kc;");
+
+            while (rs.next()) {
+                courseIDSet.add(rs.getString("courseID"));
+            }
+        }
+
+        return courseIDSet;
+    }
+}
---
+++ WEB-INF/src/ac/adproj/scms/dao/StudentDAO.java
@@ -0,0 +1,175 @@
+/*
+    Copyright (C) 2011-2020 Andy Cheung
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package ac.adproj.scms.dao;
+
+import ac.adproj.scms.entity.Entity;
+import ac.adproj.scms.entity.GenderEnum;
+import ac.adproj.scms.entity.Student;
+import ac.adproj.scms.service.photo.PhotoService;
+import ac.adproj.scms.service.photo.PhotoServiceFactory;
+import ac.adproj.scms.servlet.InitServlet;
+import ac.adproj.scms.servlet.ServletProcessingException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
+public final class StudentDAO {
+    private StudentDAO() {
+        // NOOP
+    }
+
+    /**
+     * Returns a Student object according to the DB's record.
+     *
+     * @param stuid The student ID.
+     * @return The Student object, or null if the student object did not exist.
+     */
+    public static Student getStudentObjectThroughDB(String stuid) {
+        String name;
+        GenderEnum gender;
+        String major;
+        String birthdate;
+        String remark;
+        byte[] photo;
+        int totalCredits;
+
+        try (DBDao daoO = InitServlet.daoO) {
+            ResultSet rs = daoO.query("select * from xs where stuid=?;", stuid);
+
+            if (!rs.next()) {
+                return null;
+            }
+
+            name = rs.getString("name");
+            birthdate = rs.getString("birthdate");
+            major = rs.getString("major");
+            gender = rs.getInt("gender") == 1 ? GenderEnum.MALE : GenderEnum.FEMALE;
+
+            ResultSet rs_totalC = daoO.query("select sum(credit) as c from xs_kc where stuid=? and score >= 60;"
+                    , stuid);
+
+            Blob photoBlob = rs.getBlob("photo");
+
+            InputStream is;
+
+            if (photoBlob != null) {
+                is = photoBlob.getBinaryStream();
+                photo = is.readAllBytes();
+            } else {
+                photo = new byte[]{};
+            }
+
+            remark = rs.getString("remark");
+
+            rs_totalC.next();
+            totalCredits = rs_totalC.getInt("c");
+
+            return new Student(name, birthdate, gender, stuid, photo, major, totalCredits, remark);
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+            throw new ServletProcessingException(e);
+        }
+    }
+
+    /**
+     * Upload the Student object to the database.
+     *
+     * @param s The student object that needs to upload.
+     * @throws SQLException If database ran into error.
+     */
+    public static void writeStudentObjectToDatabase(Student s) throws SQLException, IOException {
+        try (DBDao daoO = InitServlet.daoO) {
+
+            if (daoO.query("select name from xs where stuid=?", s.getId()).next()) {
+                daoO.update("update xs set name=?, major=?, gender=?, birthdate=?, "
+                                + "remark=? where stuid=?;"
+                        , s.getName()
+                        , s.getMajor()
+                        , Integer.toString(s.getGender().getGenderNumber())
+                        , s.getDob(), s.getRemark(), s.getId());
+            } else {
+                // id name major gender dob totalCredits(=0) photo remark
+                daoO.insert("insert into xs values (?, ?, ?, ?, ?, 0, NULL, ?);"
+                        , s.getId()
+                        , s.getName()
+                        , s.getMajor()
+                        , Integer.toString(s.getGender().getGenderNumber())
+                        , s.getDob()
+                        , s.getRemark());
+            }
+
+            if (s.getPhoto() != null && s.getPhoto().length != 0) {
+                updatePhoto(s.getId(), s.getPhoto());
+            }
+        }
+    }
+
+    /**
+     * Updates the photo.
+     *
+     * @param id    The student ID.
+     * @param pictW The picture array.
+     * @throws SQLException If DB ran into error.
+     */
+    private static void updatePhoto(String id, byte[] pictW) throws SQLException, IOException {
+        if (pictW != null && pictW.length != 0) {
+            PhotoService ps = PhotoServiceFactory.getPhotoService();
+            ps.uploadPhoto(id, pictW);
+        }
+    }
+
+    public static void deleteObject(String id) throws SQLException {
+        try (DBDao daoO = InitServlet.daoO) {
+            daoO.delete("delete from xs where xs.stuid=?;", id);
+        }
+    }
+
+    public static void deleteObject(Entity e) throws SQLException {
+        deleteObject(e.getId());
+    }
+
+    public static Set<String> getStudentIDSet() throws SQLException {
+        HashSet<String> studentSet = new HashSet<>();
+
+        try (DBDao daoO = InitServlet.daoO) {
+            ResultSet rs = daoO.query("select stuid from xs;");
+
+            while (rs.next()) {
+                studentSet.add(rs.getString("stuid"));
+            }
+        }
+
+        return studentSet;
+    }
+
+    public static Set<Student> getStudentObjectSet() throws SQLException {
+        Set<Student> studentSet = new HashSet<>();
+        Set<String> idSet = getStudentIDSet();
+
+        for (String id : idSet) {
+            studentSet.add(getStudentObjectThroughDB(id));
+        }
+
+        return studentSet;
+    }
+}
